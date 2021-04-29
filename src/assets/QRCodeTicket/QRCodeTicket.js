e83efc62a50cb5af10a45dbbc0e3cf764373fa22
Arnaud Puaud <puaud.arnaud@outlook.fr>
2021-04-29 14:31:57 +0200

    modif titre QR CODE

--- src/assets/QRCodeTicket/QRCodeTicket.js
+++ src/assets/QRCodeTicket/QRCodeTicket.js
@@ -3,32 +3,53 @@ import QRCode from "qrcode.react";
 import { Col, Row } from "react-bootstrap";
 import "./QrCodeTicket.css";
 import QRCODE from "easyqrcodejs";
+import qrcode from "qrcode.react";
+import QrCode from "../components/QRCode/QrCode";
 
 function QrHookTicket() {
-  const [restaurant, setRestaurant] = useState();
   let uri =
     "https://back-end.osc-fr1.scalingo.io/client/getMenuTicket?restaurantName=" +
     JSON.parse(localStorage.getItem("propsRestaurant"));
+
   useEffect(() => {
-    console.log("dsqdsq", JSON.parse(localStorage.getItem("propsRestaurant")));
-    new QRCODE(document.getElementById("qrCodeTicketRef"), {
+    new QRCODE(document.getElementById("qrCodeDiv"), {
       text: encodeURI(uri),
       width: 200,
       height: 200,
       title: "TIPOURBOIRE", // content
-      titleFont: "bold 18px Arial", //font. default is "bold 16px Arial"
+      titleFont: "bold 18px  Montserrat", //font. default is "bold 16px Arial"
       titleColor: "#f5a624", // color. default is "#000"
       titleBackgroundColor: "#fff", // background color. default is "#fff"
       titleHeight: 70, // height, including subTitle. default is 0
       titleTop: 25, // draws y coordinates. default is 30
       subTitle: "Scannez, Donnez avec Tipourboire", // content
-
-      subTitleFont: "13px Arial", // font. default is "14px Arial"
+      subTitleFont: "12px Montserrat", // font. default is "14px Arial"
       subTitleColor: "#f5a624", // color. default is "4F4F4F"
       subTitleTop: 50, // draws y coordinates. default is 0
     });
   }, []);
-  return <div id="qrCodeTicketRef" />;
+  return (
+    <div>
+      <div id="qrCodeDiv" />
+      <button
+        className="buttonQrCode"
+        onClick={() => {
+          const canvas = document.querySelector("#qrCodeDiv canvas");
+          const image = canvas.toDataURL();
+
+          const element = document.createElement("a");
+          element.setAttribute("href", image);
+          element.setAttribute("download", "canvas.png");
+
+          document.body.appendChild(element);
+
+          element.click();
+        }}
+      >
+        Télécharger le QR Code Ticket
+      </button>
+    </div>
+  );
 }
 class QrCodeTicket extends Component {
   constructor(props) {
--- src/assets/components/QRCode/QrCode.js
+++ src/assets/components/QRCode/QrCode.js
@@ -17,24 +17,48 @@ function QrHook() {
     JSON.parse(localStorage.getItem("propsRestaurant"));
   useEffect(() => {
     console.log("dsqdsq", JSON.parse(localStorage.getItem("propsRestaurant")));
-    new QRCODE(document.getElementById("qrCodeRef"), {
+    new QRCODE(document.getElementById("qrCodeDivResto"), {
       text: encodeURI(uri),
-      width: 200,
-      height: 200,
+      width: 220,
+      height: 220,
       title: "TIPOURBOIRE", // content
-      titleFont: "bold 18px Arial", //font. default is "bold 16px Arial"
+      titleFont: "bold 18px Montserrat", //font. default is "bold 16px Arial"
       titleColor: "#f5a624", // color. default is "#000"
       titleBackgroundColor: "#fff", // background color. default is "#fff"
       titleHeight: 70, // height, including subTitle. default is 0
       titleTop: 25, // draws y coordinates. default is 30
       subTitle: "Scannez, Donnez avec Tipourboire", // content
 
-      subTitleFont: "12px Arial", // font. default is "14px Arial"
+      subTitleFont: "11px Montserrat", // font. default is "14px Arial"
       subTitleColor: "#f5a624", // color. default is "4F4F4F"
       subTitleTop: 50, // draws y coordinates. default is 0
     });
   }, []);
-  return <div id="qrCodeRef" />;
+  return (
+    <div>
+      <div id="qrCodeDivResto" />
+      <button
+        className="buttonQrCode"
+        onClick={() => {
+          const canvas = document.querySelector("#qrCodeDivResto canvas");
+          const image = canvas.toDataURL();
+
+          const element = document.createElement("a");
+          element.setAttribute("href", image);
+          element.setAttribute("download", "qrcode.png");
+
+          element.style.display = "none";
+          document.body.appendChild(element);
+
+          element.click();
+
+          document.body.removeChild(element);
+        }}
+      >
+        Télécharger le QR Code Table
+      </button>
+    </div>
+  );
 }
 
 class QrCode extends Component {

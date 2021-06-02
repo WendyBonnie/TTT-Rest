d937f903d13474f4bb9184b62f36293dea8aabe2
Lambert-BC <aug.lamb@gmail.com>
2021-06-02 16:40:24 +0200

    ajout retractarion abo

--- src/components/CheckoutForm/CheckoutForm.js
+++ src/components/CheckoutForm/CheckoutForm.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
 import "./CheckoutForm.css";
 import CardSection from "../../assets/components/CardSection/CardSection";
+import Form from "react-bootstrap/Form";
 export default function CheckoutForm() {
   const stripe = useStripe();
   const elements = useElements();
@@ -50,18 +51,21 @@ export default function CheckoutForm() {
     }
     function createSubscription({ customerId, paymentMethodId, priceId }) {
       return (
-        fetch("https://back-end.osc-fr1.scalingo.io/restaurateur/createsubscription", {
-          method: "post",
-          headers: {
-            "Content-type": "application/json",
-            Authorization: "Bearer " + localStorage.getItem("token"),
-          },
-          body: JSON.stringify({
-            customerId: customerId,
-            paymentMethodId: paymentMethodId,
-            priceId: "price_1IGLo3Hoh2Vgz5QdnS6OVonj",
-          }),
-        })
+        fetch(
+          "https://back-end.osc-fr1.scalingo.io/restaurateur/createsubscription",
+          {
+            method: "post",
+            headers: {
+              "Content-type": "application/json",
+              Authorization: "Bearer " + localStorage.getItem("token"),
+            },
+            body: JSON.stringify({
+              customerId: customerId,
+              paymentMethodId: paymentMethodId,
+              priceId: "price_1IGLo3Hoh2Vgz5QdnS6OVonj",
+            }),
+          }
+        )
           .then((response) => {
             return response.json();
           })
@@ -104,7 +108,14 @@ export default function CheckoutForm() {
   return (
     <form className="coForm" onSubmit={handleSubmit}>
       <CardSection />
-      <button className="submitButton" disabled={!stripe}>
+      <Form.Check
+        className="checkboxRetRest"
+        type="checkbox"
+        name="retractation"
+        label=" Je renonce à mon droit de rétractation d'une durée de 14 jours, afin que le service commence immédiatement. "
+        required
+      />
+      <button className="submitButtonSubRestau" disabled={!stripe}>
         Devenir un restaurateur Premium
       </button>
     </form>

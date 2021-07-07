1b0c4294dd1f8b41d6b66de9b889232c6a00c2e6
Wendy-M <wendymontagnon@gmail.com>
2021-07-07 14:39:50 +0200

    maj

---
+++ build/precache-manifest.cc05347c4988e3ab6c2f79e28ab1e0db.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7cfcc32a4c2df113443e5518e038f12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e59fad5098ccf556f55f",
+    "url": "/static/css/2.8eeb7d84.chunk.css"
+  },
+  {
+    "revision": "b91badd9db080d03f819",
+    "url": "/static/css/main.e9822433.chunk.css"
+  },
+  {
+    "revision": "e59fad5098ccf556f55f",
+    "url": "/static/js/2.c017e6d7.chunk.js"
+  },
+  {
+    "revision": "861270dc56fc11dfc4b9511ee8be866b",
+    "url": "/static/js/2.c017e6d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b91badd9db080d03f819",
+    "url": "/static/js/main.9163a64b.chunk.js"
+  },
+  {
+    "revision": "704b5ac2bf7c16ab9d03",
+    "url": "/static/js/runtime-main.a96dede7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
